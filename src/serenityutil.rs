fc8ccfa6335a7c6c1035619ceae5820b9e662eed
MisileLaboratory <misileminecord@gmail.com>
2022-04-11 20:32:35 +0900

    refactor: :recycle: change name of function
    add embeds -> add fields

--- src/serenityutil.rs
+++ src/serenityutil.rs
@@ -1,7 +1,7 @@
 use serenity::builder::CreateEmbed;
 
 /// Vec<(content: String, description: String, inline: bool)>
-pub fn add_embeds(embeds: Vec<(String, String, bool)>) -> CreateEmbed {
+pub fn add_fields(embeds: Vec<(String, String, bool)>) -> CreateEmbed {
     let mut embed = CreateEmbed::default();
     embed.fields(embeds);
     return embed;
