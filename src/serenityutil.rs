9c19129549e01c2fe8dc90329f658194150d4d56
MisileLaboratory <misileminecord@gmail.com>
2022-04-16 02:04:41 +0900

    Update serenityutil.rs

--- src/serenityutil.rs
+++ src/serenityutil.rs
@@ -1,8 +1,8 @@
 use poise::serenity_prelude as serenity;
 
 /// Vec<(content: String, description: String, inline: bool)>
-pub fn add_fields(embeds: Vec<(String, String, bool)>) -> CreateEmbed {
-    let mut embed = CreateEmbed::default();
+pub fn add_fields(embeds: Vec<(String, String, bool)>) -> serenity::CreateEmbed {
+    let mut embed = serenity::CreateEmbed::default();
     embed.fields(embeds);
     return embed;
 }
